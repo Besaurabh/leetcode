668781deaec30d90f1dc2ddc8f781ce1fdd05711
Saurabh Singh <135314656+Besaurabh@users.noreply.github.com>
2024-08-25 00:50:27 +0530

    Time: 3 ms (7.36%), Space: 41.5 MB (42.11%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String toLowerCase(String s) {
+       
+        String lowercase = s.toLowerCase();  // Correct method name
+        System.out.println(lowercase);       // Added missing semicolon
+        return lowercase; 
+    }
+}
